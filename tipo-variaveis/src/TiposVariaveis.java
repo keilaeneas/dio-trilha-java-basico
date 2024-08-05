ccaa82564100324a71e95ff7525b31e1e437406f
Keila Nayara Enéas <keilaeneas@gmail.com>
2024-08-05 04:07:08 -0300

    Feito exercicios de Documentação Java

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int a, b;
+        a = 5;
+        b = 6;
+
+        int resultado = a==b ? 1 : 0;
+
+        System.out.println(resultado);
     }
 }
