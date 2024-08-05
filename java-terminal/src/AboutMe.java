21325aeaa32e475751a444c923dd1c741a37e216
Keila Nayara Enéas <keilaeneas@gmail.com>
2024-08-05 04:43:58 -0300

    Meu projeto conta banco

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,10 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
 
-        String nome = args [0];
+        /*String nome = args [0];
         String sobrenome = args [1];
         int idade = Integer.valueOf(args[2]);
         double altura = Double.valueOf(args[3]);
+        */
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome ");
+        String nome = sc.next();
+
+        System.out.println("Digite seu sobrenome ");
+        String sobrenome = sc.next();
+
+        System.out.println("Digite sua idade ");
+        int idade = sc.nextInt();
+
+        System.out.println("Digite sua altura ");
+        double altura = sc.nextDouble();
 
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
